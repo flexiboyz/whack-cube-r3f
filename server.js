1ee34211fc4e35733cda59048d830a0762391563
Rone <flexiboyz@gmail.com>
2026-02-18 22:56:07 +0100

    Convert server.js to ES modules (fix Node.js compatibility)

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173", // Vite dev server
